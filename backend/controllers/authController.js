25ea7335a7402266ebae684a5b7cce5ba4932a6a
MrKabado <jersonjaybonghanoy@gmail.com>
2025-08-24 17:27:17 +0800

    login register implementation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,25 +1,44 @@
 import usersModel from "../models/usersSchema.js";
-
-export const authController = (req, res) => {
-  const { email, password } = req.body;
-
-  console.log(email, password)
-  return res.json({ success: true })
-}
+import bcrypt from 'bcrypt';
 
 export const createUsers = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { name, email, password } = req.body;
+    
+    const alreadyRegistered = await usersModel.findOne({ email })
+    if (alreadyRegistered) {
+      return res.status(401).json({success: false, message: "This email is already registered"});
+    }
 
-    const registeredUser = new usersModel({email, password});
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const registeredUser = new usersModel({name, email, password: hashedPassword});
     await registeredUser.save();
     res.status(201);
     res.json({
       success: true,
-      users: registeredUser
+      users: registeredUser,
+      message: "Registered Successfully"
     })
 
   } catch (error) {
     res.status(500).json({error: error.message})
   }
+}
+
+export const checkUsers = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersModel.findOne({email});
+  if (!user) return res.status(404).json({message: "User not found"});
+
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(401).json({success: false, message: "Invalid Password"});
+  }
+
+  return res.json({success: true, message: "Login Successfully", user: user.name});
 }
--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { createUsers } from '../controllers/authController.js'
+import { createUsers, checkUsers } from '../controllers/authController.js'
 
 const users = express.Router()
 
-users.post('/registered-user', createUsers)
+users.post('/registered-user', createUsers);
+
+users.post('/login-user', checkUsers);
 
 export default users
